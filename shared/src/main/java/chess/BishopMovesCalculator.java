a2d0dc3a09c1a2777dc3bcafb8b2ba6121238ad6
Colton <coltongsessions@gmail.com>
2025-01-21 19:59:17 -0700

    Implement QueenMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -10,98 +10,33 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
         int startCol = position.getColumn();
         int testRow = startRow;
         int testCol = startCol;
+        int[][] directions = {{1,-1}, {1,1}, {-1,1}, {-1,-1}};
         ChessMove newMove;
         ChessPosition checker;
         ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
         ChessPiece pieceChecked;
-
-        while(testRow < 8 && testCol < 8){
-            testRow++;
-            testCol++;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
+        for (int i = 0; i < 4; i++){
+            testRow = startRow;
+            testCol = startCol;
+            while(testCol >= 1 && testCol <= 8 && testRow >= 1 && testRow <= 8) {
+                testRow = testRow + directions[i][0];
+                testCol = testCol + directions[i][1];
+                if (testCol >= 1 && testCol <= 8 && testRow >= 1 && testRow <= 8) {
+                    checker = new ChessPosition(testRow, testCol);
+                    pieceChecked = board.getPiece(checker);
+                    if (pieceChecked == null) {
+                        newMove = new ChessMove(position, checker, null);
+                        moves.add(newMove);
+                    } else if (pieceChecked.getTeamColor() != color) {
+                        newMove = new ChessMove(position, checker, null);
+                        moves.add(newMove);
+                        break;
+                    } else if(pieceChecked.getTeamColor() == color ){
+                        break;
+                    }
+                }
             }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
         }
-        testRow = startRow;
-        testCol = startCol;
-
-        while(testRow > 1 && testCol < 8){
-            testRow--;
-            testCol++;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
-
-        }
-        testRow = startRow;
-        testCol = startCol;
-
-        while(testCol > 1 && testRow < 8){
-            testCol--;
-            testRow++;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
-        }
-        testCol = startCol;
-
-        while(testCol > 1 && testRow > 1) {
-            testCol--;
-            testRow--;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if (pieceChecked == null) {
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            } else if (pieceChecked.getTeamColor() != color) {
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            } else if (pieceChecked.getTeamColor() == color) {
-                break;
-            }
-
-        }
-
         return moves;
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -11,92 +11,33 @@ public class RookMovesCalculator implements PieceMovesCalculator {
         int startCol = position.getColumn();
         int testRow = startRow;
         int testCol = startCol;
+        int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
         ChessMove newMove;
         ChessPosition checker;
         ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
         ChessPiece pieceChecked;
-
-        while(testRow < 8){
-            testRow++;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
+        for (int i = 0; i < 4; i++){
+            testRow = startRow;
+            testCol = startCol;
+            while(testCol >= 1 && testCol <= 8 && testRow >= 1 && testRow <= 8) {
+                testRow = testRow + directions[i][0];
+                testCol = testCol + directions[i][1];
+                if (testCol >= 1 && testCol <= 8 && testRow >= 1 && testRow <= 8) {
+                    checker = new ChessPosition(testRow, testCol);
+                    pieceChecked = board.getPiece(checker);
+                    if (pieceChecked == null) {
+                        newMove = new ChessMove(position, checker, null);
+                        moves.add(newMove);
+                    } else if (pieceChecked.getTeamColor() != color) {
+                        newMove = new ChessMove(position, checker, null);
+                        moves.add(newMove);
+                        break;
+                    } else if(pieceChecked.getTeamColor() == color ){
+                        break;
+                    }
+                }
             }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
         }
-        testRow = startRow;
-
-        while(testRow > 1){
-            testRow--;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
-
-        }
-        testRow = startRow;
-
-        while(testCol < 8){
-            testCol++;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if(pieceChecked == null){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            }
-            else if(pieceChecked.getTeamColor() != color ){
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            }
-
-            else if(pieceChecked.getTeamColor() == color ){
-                break;
-            }
-
-        }
-        testCol = startCol;
-
-        while(testCol > 1) {
-            testCol--;
-            checker = new ChessPosition(testRow, testCol);
-            pieceChecked = board.getPiece(checker);
-            if (pieceChecked == null) {
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-            } else if (pieceChecked.getTeamColor() != color) {
-                newMove = new ChessMove(position, checker, null);
-                moves.add(newMove);
-                break;
-            } else if (pieceChecked.getTeamColor() == color) {
-                break;
-            }
-
-        }
-
         return moves;
     }
 
