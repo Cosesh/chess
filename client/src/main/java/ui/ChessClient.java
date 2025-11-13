54e50ce0806caba15c1c786a52befbf93dfd1724
Colton Sessions <coltongsessions@gmail.com>
2025-11-12 23:51:54 -0700

    Repl working with main

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,67 @@
+package ui;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class ChessClient {
+
+
+    public ChessClient(String serverUrl)  {
+
+    }
+
+    public void run() {
+        System.out.println( " Welcome to Colton's Chess Lobby. Press enter for options");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "login" -> login(params);
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+
+    public String help() {
+        return """
+                - login
+                - register
+                - quit
+                - help
+                """;
+    }
+
+    public String login (String... params) {
+
+        return "you signed in";
+    }
+
+    private void printPrompt() {
+        System.out.print("\n"  + ">>> " );
+    }
+
+}
