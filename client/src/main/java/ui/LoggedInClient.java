677f04dba4aea06a0361bd63aad7538e183e7ab8
Colton Sessions <coltongsessions@gmail.com>
2025-11-13 19:15:18 -0700

    printBoardWhite

--- client/src/main/java/ui/LoggedInClient.java
+++ client/src/main/java/ui/LoggedInClient.java
@@ -1,5 +1,6 @@
 package ui;
 
+import static ui.EscapeSequences.*;
 import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessPosition;
@@ -8,6 +9,7 @@ import model.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class LoggedInClient {
 
@@ -89,35 +91,70 @@ public class LoggedInClient {
         ChessBoard board = new ChessBoard();
         board.resetBoard();
         var toPrint = boardString(board);
-        printBoard(toPrint);
+        printBoardWhite(toPrint);
         return "";
     }
 
 
     private String[][] boardString(ChessBoard board) {
         String[][] boardString = new String[8][8];
+        boolean isWhite = true;
         for (int i = 1; i <= 8 ; i++) {
             for (int j = 1; j <= 8; j++) {
                 ChessPosition pos = new ChessPosition(i,j);
                 var piece = board.getPiece(pos);
                 if(piece == null){
-                    boardString[i-1][j-1] = "!";
-                }else {
-                    boardString[i-1][j-1] = piece.getPieceType().name().substring(0,1);
-                }
+                    if(isWhite){
+                        boardString[i-1][j-1] = SET_BG_COLOR_WHITE + "   " + RESET_BG_COLOR;
+                    } else {
+                        boardString[i-1][j-1] = SET_BG_COLOR_BLACK + "   " + RESET_BG_COLOR;
+                    }
 
+                }else {
+                    var type = piece.getPieceType();
+                    var color = piece.getTeamColor();
+                    var letter = "";
+                    if(type.name().equals("KNIGHT")) {
+                        letter = "N";
+                    } else{
+                        letter = type.name().substring(0,1);
+                    }
+                    setColors(isWhite, color, boardString, i, j, letter);
+                }
+                isWhite = !isWhite;
             }
+            isWhite = !isWhite;
         }
         return boardString;
     }
 
-    private void printBoard(String[][] toPrint) {
-        for (int i = 0; i < toPrint.length; i++) {
-            for (int j = 0; j <toPrint[i].length ; j++) {
-                System.out.print(toPrint[i][j] + "  ");
+    private static void setColors(boolean isWhite, ChessGame.TeamColor color, String[][] boardString, int i, int j, String letter) {
+        if(isWhite){
+            if(color.equals(ChessGame.TeamColor.BLACK)){
+                boardString[i -1][j -1] = SET_BG_COLOR_WHITE + " " +  SET_TEXT_COLOR_BLUE + letter + " " + RESET_BG_COLOR;
+            } else {
+                boardString[i -1][j -1] = SET_BG_COLOR_WHITE + " " + SET_TEXT_COLOR_RED + letter + " "  + RESET_BG_COLOR;
             }
+        } else {
+            if(color.equals(ChessGame.TeamColor.BLACK)){
+                boardString[i -1][j -1] = SET_BG_COLOR_BLACK  + " " + SET_TEXT_COLOR_BLUE + letter + " " + RESET_BG_COLOR;
+            } else {
+                boardString[i -1][j -1] = SET_BG_COLOR_BLACK + " " + SET_TEXT_COLOR_RED + letter + " " +  RESET_BG_COLOR;
+            }
+        }
+    }
+
+    private void printBoardWhite(String[][] toPrint) {
+        System.out.println( SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE +"    h  g  f  e  d  c  b  a    " + RESET_TEXT_COLOR);
+        for (int i = 0; i < toPrint.length; i++) {
+            System.out.print(SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE +" " + (i+1) + " " + RESET_TEXT_COLOR);
+            for (int j = 0; j <toPrint[i].length ; j++) {
+                System.out.print(toPrint[i][j]);
+            }
+            System.out.print(SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE +" " + (i+1) + " " + RESET_TEXT_COLOR);
             System.out.println();
         }
+        System.out.println(SET_TEXT_BOLD + SET_TEXT_COLOR_WHITE +"    h  g  f  e  d  c  b  a    " + RESET_TEXT_COLOR);
     }
 
 
